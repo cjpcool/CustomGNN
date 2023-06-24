43bdd09c32768e151ebb3ec5bd5d168b37f991a2
Jianpeng Chen <37957296+cjpcool@users.noreply.github.com>
2023-06-24 16:53:13 +0800

    Delete train_citeseer.sh

--- train_citeseer.sh
+++
@@ -1,1 +0,0 @@
-python custom_gnn.py --cuda_device 0 --dataset 'citeseer' --lam_pw_emd 10 --order 4 --path_length 10 --window_size 5 --dropout_enc 0.6 --lstm_hidden_units 128 --batch_size 300 --seed 77 --schedule_patience 100 --learning_rate 0.01 --lam_tri 0.1 --lam_tri_lstm 1.0 --margin 1.0
