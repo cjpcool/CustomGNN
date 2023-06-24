b72db95e717d508fc6b07f6e346416461b424e00
Jianpeng Chen <37957296+cjpcool@users.noreply.github.com>
2023-06-24 16:53:05 +0800

    Delete test_cora.sh

--- test_cora.sh
+++
@@ -1,1 +0,0 @@
-python custom_gnn.py --cuda_device 1 --only_test True --dataset 'cora' --para_name 'cora_example' --lam_pw_emd 1.0 --pw_adj_name 'cora_example' --embedding_dim 512 --path_length 10 --window_size 10 --lstm_hidden_units 128
